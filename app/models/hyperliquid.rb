efb2f2fef092440ecdd9b58c8bca058de5076185
AJ Villalobos <hello@ajvillalobos.com>
2024-04-24 17:53:41 +0800

    Display funding rate for Hyperliquid

--- app/models/hyperliquid.rb
+++ app/models/hyperliquid.rb
@@ -8,4 +8,19 @@ class Hyperliquid
     body = { "type" => "predictedFundings" }
     self.class.post("/info", headers: headers, body: body.to_json)
   end
+
+  def save_funding_rate(response, exchange, instrument)
+    token_details = response.find { |data| data[0] == instrument.name }
+
+    return unless token_details
+
+    perp_data = token_details.last.find { |data| data[0] == "HlPerp" }
+
+    FundingRate.create!(
+      rate: perp_data.last["fundingRate"],
+      expire_at: milliseconds_to_datetime(perp_data.last["nextFundingTime"]),
+      exchange: exchange,
+      instrument: instrument
+    )
+  end
 end
