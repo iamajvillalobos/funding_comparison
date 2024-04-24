8008a2eddea0ad54ee65e44455aa44005597dfab
AJ Villalobos <hello@ajvillalobos.com>
2024-04-24 18:03:53 +0800

    Show arb rate

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @instruments = Instrument.order(name: :asc)
+    @aevo = Exchange.find_by(name: "Aevo")
+    @hyperliquid = Exchange.find_by(name: "Hyperliquid")
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     klass = rate.positive? ? 'text-green-700' : 'text-red-700'
     content_tag(:span, number_to_percentage(rate.abs, precision: 6), class: klass)
   end
+
+  def calculate_arb(rate1, rate2)
+    return '-' if rate1.nil? || rate2.nil?
+    display_rate_with_color(rate1 - rate2)
+  end
 end
