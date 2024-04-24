229b5c843d3dac0c7e6141fc17d6908f0fed842c
AJ Villalobos <hello@ajvillalobos.com>
2024-04-24 13:47:01 +0800

    Show aevo current hourly rate

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,5 @@
 class DashboardsController < ApplicationController
   def show
+    @instruments = Instrument.order(name: :asc)
   end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,20 @@
 class Instrument < ApplicationRecord
   has_many :funding_rates
+
+  def hourly_rate(exchange)
+    time = Time.now
+
+    funding_rate = FundingRate.find_by(
+      exchange: exchange,
+      instrument: self,
+      created_at: time.beginning_of_hour..time.end_of_hour
+    )
+
+    if funding_rate
+      funding_rate.rate
+    else
+      FetchFundingRateJob.perform_later(exchange: exchange, instrument: self)
+      0.0
+    end
+  end
 end
