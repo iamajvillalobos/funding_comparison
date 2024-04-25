62f10f9ddeafb33a44770705d62a7054acac1989
AJ Villalobos <hello@ajvillalobos.com>
2024-04-25 23:54:58 +0800

    Remove unused code

--- app/models/aevo.rb
+++ app/models/aevo.rb
@@ -3,30 +3,13 @@ class Aevo
   include HTTParty
   base_uri 'api.aevo.xyz'
 
-  Rate = Struct.new(:rate, :expire_at)
-
   # returns the current funding rate for the instrument
   def funding(instrument)
     self.class.get("/funding", query: { instrument_name: "#{instrument}-PERP"})
   end
 
-  def funding_history(instrument, start_time, end_time, limit = 50)
-    instrument_name = "#{instrument}-PERP"
-    start_time_epoch = time_to_nanoseconds(start_time)
-    end_time_epoch = time_to_nanoseconds(end_time)
-
-    query = {
-      instrument_name: instrument_name,
-      start_time: start_time_epoch,
-      end_time: end_time_epoch,
-      limit: limit
-    }
-
-    response = self.class.get("/funding-history", query: query)
-    parse_response(response)
-  end
-
   def save_funding_rate(response, exchange, instrument)
+    binding.irb
     FundingRate.create!(
       rate: response["funding_rate"],
       expire_at: nanoseconds_to_time(response["next_epoch"]),
@@ -34,15 +17,4 @@ class Aevo
       instrument: instrument
     )
   end
-
-  private
-
-  def parse_response(response)
-    return response if response["funding_history"].empty?
-
-    response["funding_history"].map do |sym, time, rate, price|
-      Rate.new(rate: rate, expire_at: nanoseconds_to_time(time))
-    end
-  end
-
 end
