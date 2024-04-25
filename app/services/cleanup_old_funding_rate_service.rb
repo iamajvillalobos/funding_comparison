b68f9b68daceb0d5aa9a1feb3866d8c3de32c234
AJ Villalobos <hello@ajvillalobos.com>
2024-04-26 07:27:15 +0800

    Shorten time for deleting old rates

--- app/services/cleanup_old_funding_rate_service.rb
+++ app/services/cleanup_old_funding_rate_service.rb
@@ -1,5 +1,5 @@
 class CleanupOldFundingRateService
   def self.run(instrument)
-    instrument.funding_rates.where("created_at < ?", 1.hour.ago).delete_all
+    instrument.funding_rates.where("created_at < ?", 12.minutes.ago).delete_all
   end
 end
