5d6ebd6cd7db3f96295605e4b256f3b9e1fa53e4
AJ Villalobos <hello@ajvillalobos.com>
2024-04-25 23:54:49 +0800

    Load the latest rate properly

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -4,11 +4,11 @@ class Instrument < ApplicationRecord
   def hourly_rate(exchange)
     time = Time.now
 
-    funding_rate = FundingRate.find_by(
+    funding_rate = FundingRate.where(
       exchange: exchange,
       instrument: self,
       created_at: time.beginning_of_hour..time.end_of_hour
-    )
+    ).last
 
     funding_rate ? funding_rate.rate : 0
   end
