f0536016dab2942969dcbdfa9091cdb597fe3d9b
AJ Villalobos <hello@ajvillalobos.com>
2024-04-25 23:26:59 +0800

    Add filtering via coin params

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,10 @@
 class DashboardsController < ApplicationController
   def show
-    @instruments = Instrument.order(name: :asc)
+    @instruments = if params[:coin]
+      Instrument.where(name: params[:coin])
+    else
+      Instrument.order(name: :asc)
+    end
     @aevo = Exchange.find_by(name: "Aevo")
     @hyperliquid = Exchange.find_by(name: "Hyperliquid")
     @latest_funding_rate = FundingRate.order(created_at: :desc)&.first
